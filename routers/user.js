d7641b2035ccc1d6efb77a159278df2d4cf2f24e
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-03-19 17:04:25 +0300

    Add files via upload

---
+++ routers/user.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const UserController = require('../controllers/user-controller')
+const user_controller = new UserController()
+
+const router = new express.Router()
+router.delete('/:id', user_controller.deleteUser)
+router.post('/',user_controller.addUser)
+router.put('/', user_controller.updateUser)
+router.get('/', user_controller.getUser)
+
+console.log(router);
+module.exports = router
